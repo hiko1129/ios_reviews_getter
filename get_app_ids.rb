aecab378721232b1e4c84e3fe6e8e0c80cdde5f8
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2016-08-29 22:45:50 +0900

    Update

--- get_app_ids.rb
+++ get_app_ids.rb
@@ -1,53 +1,165 @@
+# frozen_string_literal: truerequire 'sqlite3'
 $LOAD_PATH << '.'
 require 'request_reviews'
 require 'uri'
 require 'open-uri'
 require 'json'
+require 'pp'
 
-# パラメータ付きのリクエストURLを生成
-def create_request_url(url, params_hash)
-  temp_array = []
-  params_hash.each do |k, v|
-    temp_array << "#{k}=#{v}" unless v.nil?
+# IDの取得を行う
+class IDGetter
+  def initialize(type)
+    @type = type
+    @request = Request.new
   end
-  params = temp_array.join('&')
+  # TODO
+  # IDを取得する部分とランキングを取得する部分を分ける
 
-  request_url = "#{url}?#{params}"
-  request_url
+  def get_ids(term, offset)
+    classification = @type.value
+    return get_app_ids(term, offset) if classification == 'apps'
+    # return get_audiobook_id(term) if classification == 'audiobooks'
+    return get_ebook_ids(term, offset) if classification == 'ebooks'
+    # return get_movie_id(term) if classification == 'topmovies'
+    # return get_song_id(term) if classification == 'songs'
+  end
+
+  private
+
+  def configure_request_params(term, media, entity = nil)
+    entity = media if entity.nil?
+    {
+      term: URI.encode(term), country: 'jp', media: media,
+      entity: entity, attribute: nil, callback: nil,
+      limit: 1, offset: nil, lang: 'ja_jp'
+    }
+  end
+
+  def get_app_ids(term, offset)
+    params = configure_request_params(term, 'software')
+    params[:offset] = offset
+    @request.send(create_request_url(params))
+  end
+
+  # def get_audiobook_id(term)
+  #   params = configure_request_params(term, 'audiobook')
+  #   @request.send(create_request_url(params))
+  # end
+
+  def get_ebook_ids(term, offset)
+    params = configure_request_params(term, 'ebook')
+    params[:offset] = offset
+    @request.send(create_request_url(params))
+  end
+
+  # def get_movie_id(term)
+  #   params = configure_request_params(term, 'movie')
+  #   @request.send(create_request_url(params))
+  # end
+
+  # def get_song_id(term)
+  #   params = configure_request_params(term, 'music', 'song')
+  #   @request.send(create_request_url(params))
+  # end
+
+  # パラメータ付きのリクエストURLを生成
+  def create_request_url(params_hash)
+    url = 'https://itunes.apple.com/search'
+    temp_array = []
+    params_hash.each do |k, v|
+      temp_array << "#{k}=#{v}" unless v.nil?
+    end
+    params = temp_array.join('&')
+    "#{url}?#{params}"
+  end
 end
 
-# apiにリクエストを送信して、レスポンスとしてjsonを受け取る
-def return_response(request_url)
-  res = open(request_url)
-  code, message = res.status
+# Rankingの処理を行う
+class RankProcessor
+  def initialize
+    @type = Type.new
+  end
 
-  if code == '200'
-    response_body = JSON.parse(res.read)
-  else
+  def get_ranking(input_type)
+    feed_type = @type.find(input_type)
+    @ranking = configure_url_params(feed_type)
+    nil
+  end
+
+  def find_id
+    id_getter = IDGetter.new(@type)
+    reviews_processor = ReviewsProcessor.new
+    parse_ranking(id_getter, reviews_processor)
+  end
+
+  private
+
+  def parse_ranking(id_getter, reviews_processor)
+    # pp @ranking
+    @ranking['feed']['entry'].each do |data|
+      # pp data['im:name']['label']
+      content = id_getter.get_ids(data['im:name']['label'], 0)
+      content['results'].each do |result|
+        puts result['trackName']
+        reviews_processor.send_request(result['trackId'])
+        reviews_processor.show
+      end
+      show_notice
+    end
+  end
+
+  def show_notice
+    puts 'wait 3 seconds'
+    sleep(3)
+  end
+
+  def configure_url_params(feed_type)
+    base_url = 'https://itunes.apple.com/jp/rss'
+    size = '/limit=200'
+    file_type = '/json'
+    rss_feed_url = base_url + feed_type + size + file_type
+    request = Request.new
+    request.send(rss_feed_url)
+  end
+end
+
+# Requestを送信するのみ
+class Request
+  # apiにリクエストを送信して、レスポンスとしてjsonを受け取る
+  def send(request_url)
+    res = open(request_url)
+    code, message = res.status
+    return JSON.parse(res.read) if code == '200'
     puts "#{code} #{message}"
-    response_body = nil
   end
-  response_body
 end
 
-# アプリケーションに関するデータを取得する
-# アプリケーションのIDを取得するためのメソッド
-def get_app_ids(term)
-  base_url = 'https://itunes.apple.com/search'
-  params = {
-    term: URI.encode(term), country: 'jp', media: 'software',
-    entity: 'software', attribute: nil, callback: nil,
-    limit: 1, offset: nil, lang: 'ja_jp'
-  }
-  return_response(create_request_url(base_url, params))
-end
+# 種類、分類の処理を行い保持する
+class Type
+  attr_reader :value
 
-# アプリケーションのランキングを取得する
-def acquire_top_free_apps
-  base_url = 'https://itunes.apple.com/jp/rss'
-  feed_type = '/topfreeapplications'
-  size = '/limit=200'
-  file_type = '/json'
-  rss_feed_url = base_url + feed_type + size + file_type
-  return_response(rss_feed_url)
+  def find(type)
+    @value = type.to_s.split('_')[-1]
+    classify(@value, type)
+  end
+
+  private
+
+  def classify_apps(type)
+    return '/topfreeapplications' if type == :top_free_apps
+    '/toppaidapplications'
+  end
+
+  def classify_books(type)
+    return '/topfreeebooks' if type == :top_free_ebooks
+    # '/toppaidebooks'
+  end
+
+  def classify(classification, type)
+    return classify_apps(type) if classification == 'apps'
+    # return '/topaudiobooks' if classification == 'audiobooks'
+    return classify_books(type) if classification == 'ebooks'
+    # return '/topmovies' if classification == 'movies'
+    # return '/topsongs' if classification == 'songs'
+  end
 end
--- request_reviews.rb
+++ request_reviews.rb
@@ -1,49 +1,58 @@
+# frozen_string_literal: truerequire 'sqlite3'
 require 'open-uri'
 require 'json'
+require 'pp'
 
-# リクエストを送信する
-def send_request(ios_app_id)
-  url = "https://itunes.apple.com/jp/rss/customerreviews/id=#{ios_app_id}/json"
-  response = open(url)
-  confirm_response(response)
-end
+# レビューを取得する
+class ReviewsProcessor
+  # リクエストを送信する
+  def send_request(id)
+    base_url = 'https://itunes.apple.com'
+    url = "#{base_url}/jp/rss/customerreviews/id=#{id}/json"
+    response = open(url)
+    @review_data = confirm_response(response)
+  end
 
-# レスポンスを確認する
-def confirm_response(response)
-  code, message = response.status
-  if code.to_i == 200
-    result = response.read
-    # save_reviews(result)
-    extract_review(result)
-  else
-    puts "#{code} #{message}"
+  # レスポンスを確認する
+  def confirm_response(response)
+    code, message = response.status
+    if code.to_i == 200
+      result = response.read
+      # save_reviews(result)
+      extract_review(result)
+    else
+      puts "#{code} #{message}"
+    end
+  end
+
+  # TODO
+  # 分類に応じてキーが異なるため対処する
+  # appsとtopfreebooksは問題ない
+  # レビューを抽出する
+  def extract_review(result)
+    reviews = JSON.parse(result)
+    review_data = []
+    reviews['feed']['entry'].each do |review|
+      rate = fetch_review(review, 'im:rating', 'label')
+      title = fetch_review(review, 'title', 'label')
+      content = fetch_review(review, 'content', 'label')
+      review_data << { rate: rate, title: title, content: content }
+    end
+    review_data
+  end
+
+  # nilを確認する部分の抽象化を行う
+  def fetch_review(review, key_1, key_2)
+    review[key_1][key_2] unless review[key_1].nil?
+  end
+
+  def show
+    puts 'review data is nil' if @review_data.nil?
+    @review_data.each do |h|
+      puts "rate:#{h[:rate]}"
+      puts "title:#{h[:title]}"
+      puts "content:#{h[:content]}"
+      puts
+    end
   end
 end
-
-# レビューを抽出する
-def extract_review(result)
-  reviews = JSON.parse(result)
-  review_data = []
-  reviews['feed']['entry'].each do |review|
-    rate = fetch_review(review, 'im:rating', 'label')
-    title = fetch_review(review, 'title', 'label')
-    content = fetch_review(review, 'content', 'label')
-    review_data << { rate: rate, title: title, content: content }
-  end
-  review_data
-end
-
-# nilを確認する部分の抽象化を行う
-def fetch_review(review, key_1, key_2)
-  data = review[key_1][key_2] unless review[key_1].nil?
-  data
-end
-
-# レビュー全体（レスポンス）を保存する
-# def save_reviews(result)
-#   file_path = './reviews.json'
-#   open(file_path, 'w') do |io|
-#     io.write(result)
-#     puts 'saved'
-#   end
-# end
